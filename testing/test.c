bfdf0be6d401225d66b2f6341b615db179f40358
fkerslag <fkerslag@ucsc.edu>
2014-05-01 19:52:56 -0700

    Replaced old testing program
    new one works as previously described

--- testing/test.c
+++ testing/test.c
@@ -1,17 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <limits.h>
 
 main (int argc, char *argv[]) {
-	int i = 0;
-	int j = 0;
+	int i = 0, j = 0, pid, max_loop = 595000000;
+
+	pid = getpid ();
 
 	while(1){
 		i++;
-		if(i == 1000000000){
-			printf("Loop finished\n");
+		if(i == max_loop){
+			printf("PID:%d Loop finished\n", pid);
+			i = 0;
 			j++;
 		}
-		if (j == 1000000) {
-			printf("Program finished\n");			
+		if (j == 10) {
+			printf("PID:%d Program finished\n", pid);			
 			break;
 		}
 	}
