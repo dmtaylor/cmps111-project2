5f8718931956e0ee341b32e10f39f7f5fc2a69f1
David Taylor <dmtaylor2011@gmail.com>
2014-05-02 20:30:52 -0700

    Finished script, copys files and installs

--- copysched.sh
+++ copysched.sh
@@ -4,3 +4,7 @@
 # Automates copying MINIX scheduler files
 
 cp servers/pm/scheduler.c /usr/src/servers/pm/scheduler.c
+cp servers/sched/scheduler.c /usr/src/servers/sched/scheduler.c
+cp servers/sched/schedproc.h /usr/src/servers/sched/schedproc.h
+cd /usr/src/tools
+make install
